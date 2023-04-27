dc302b98027371313984e2ef91c28adb18935c1f
LAKSHMI PRASANNA TANGUTURI <88602508+prasanna61@users.noreply.github.com>
2023-04-27 13:36:51 +0530

    Add files via upload

---
+++ query2.sql
@@ -0,0 +1,20 @@
+-- creating database
+-- create database Herovired;
+-- create table Rgm (
+-- student_name varchar(30) not null,
+-- college_name varchar(30) not null,
+-- session_name varchar(30) not null,
+-- contact_number int primary key not null,
+-- address varchar(30) not null
+-- );
+Insert into Rgm Values ("prasanna",'rgmcet','sql',709397,'Nandyal');
+insert into rgm (student_name,college_name,session_name,contact_number,address)
+values('nandu','sve','sql',9683752,'Tirupathi');
+insert into rgm values('Keerthana','rgm','sql',456712,'Nandyal');
+insert into rgm values('Kavya','rgm','sql',786314,'Nandyal');
+insert into rgm values('keerthi','rgm','sql',78963,'Nandyal');
+insert into rgm values('nandu','rgm','sql',896246,'Nandyal');
+insert into rgm values('nani','rgm','sql',775222,'Nandyal');
+insert into rgm values('teja','rgm','sql',7896377,'Nandyal');
+insert into rgm values('navitha','rgm','sql',756863,'Nandyal');
+select * from Rgm
